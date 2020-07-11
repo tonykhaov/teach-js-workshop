e66155d97a9a9800eb2c9129773b0cdea03897dd
Tony Khaov <tony.khaov@gmail.com>
2020-07-11 20:06:19 +0200

    finish the copy code

--- copyPaste/main.js
+++ copyPaste/main.js
@@ -1,1 +1,9 @@
 const copyButton = document.querySelector("button[id=copy]");
+const copyText = document.querySelector(".copy input");
+
+const copyElement = () => {
+  copyText.select();
+  document.execCommand("copy");
+};
+
+copyButton.addEventListener("click", copyElement);

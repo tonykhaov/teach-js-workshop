d61aab9b888d6bf1e860d79db38451527c091ed6
Tony Khaov <tony.khaov@gmail.com>
2020-08-01 21:20:07 +0200

    :construction: Code the src dev

---
+++ learnWebpack/src/app.js
@@ -0,0 +1,4 @@
+import films, { tomHanksFilms } from "./films";
+
+console.log("films", films);
+console.log("tomHanksFilms", tomHanksFilms);
---
+++ learnWebpack/src/films.js
@@ -0,0 +1,3 @@
+export default ["Forrest Gump", "The Big Short", "Scarface", "The Green Mile"];
+
+export const tomHanksFilms = ["Forrest Gump", "The Green Mile"];

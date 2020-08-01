8f12281ae93ba1aecb21fdc8802f4e7f5e47090f
Tony Khaov <tony.khaov@gmail.com>
2020-08-01 21:23:45 +0200

    :rocket: Show a "library/app" package from npm use

--- learnWebpack/src/app.js
+++ learnWebpack/src/app.js
@@ -1,3 +1,10 @@
+// npm = app store for js
+import isOdd from "is-odd";
+
+console.log("is 3 odd?", isOdd(3));
+console.log("is 10 odd?", isOdd(10));
+
+// webpack for imports/exports handling
 import films, { tomHanksFilms } from "./films";
 
 console.log("films", films);
